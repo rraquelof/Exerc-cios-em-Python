fc8f58d83f2ba25a6de2e10c1d44e92554d4477c
Raquel Oliveira Ferreira <rraquelof@gmail.com>
2024-08-31 19:39:10 -0300

    1.py
    Questão 1: Escreva um programa que leia três números inteiros e calcule a sua média aritmética.

---
+++ 1.py
@@ -0,0 +1,9 @@
+# Escreva um programa que leia três números inteiros e calcule a sua média aritmética.
+
+print("INFORME 3 NÚMEROS INTEIROS")
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+media = (num1 + num2 + num3)/3
+print(f"A média entre {num1}, {num2} e {num3} é igual a {media}")
