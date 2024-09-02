53d3790707116ccb9af67d7c0a427efd855feb36
Raquel Oliveira Ferreira <rraquelof@gmail.com>
2024-09-02 10:08:08 -0300

    6.py
    Escreva um programa que leia um número inteiro e informe o seu antecessor e o seu sucessor.

---
+++ 6.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia um número inteiro e informe o seu antecessor e o seu
+# sucessor.
+
+num1 = int(input("Digite um número inteiro: "))
+
+print(f"Sucessor de {num1}: {num1+1}")
+print(f"Antecessor de {num1}: {num1-1}")
